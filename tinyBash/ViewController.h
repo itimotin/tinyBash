601070053f978a736ad816cb5d3a33dd72af3e4e
Viktor Kotseruba <barbuzaster@gmail.com>
2012-09-04 21:03:53 +0800

    format

--- tinyBash/ViewController.h
+++ tinyBash/ViewController.h
@@ -11,13 +11,8 @@
 #import <SVProgressHUD/SVProgressHUD.h>
 #import <GDataXML-HTML/GDataXMLNode.h>
 #import "GTMNSString+HTML.h"
+#import "config.h"
 
-#define RSS_URL @"http://bash.im/rss/"
-#define TEXT_MARGIN 10.0f
-#define CELL_MARGIN 10.0f
-#define FONT_NAME @"Ubuntu-Light"
-#define IPHONE_FONT_SIZE 18.0f
-#define IPAD_FONT_SIZE 24.0f
 
 @interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate> {
   IBOutlet UITableView *tableView;
---
+++ tinyBash/config.h
@@ -0,0 +1,7 @@
+
+#define RSS_URL @"http://bash.im/rss/"
+#define TEXT_MARGIN 10.0f
+#define CELL_MARGIN 10.0f
+#define FONT_NAME @"Ubuntu-Light"
+#define IPHONE_FONT_SIZE 18.0f
+#define IPAD_FONT_SIZE 24.0f
